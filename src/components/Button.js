e824699ab19e5305fb8d7125e62433fc683c9450
Eric Bishard <e.bishard@gmail.com>
2018-06-26 20:24:42 -0700

    format button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,14 +1,14 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 import './Button.css';
 
 class Button extends Component {
-    render() {
-        return (
-          <button value={this.props.city} onClick={this.props.handleClick} className="city-button">
-            {this.props.city ? this.props.city : "All cities"}
-          </button>
-      );
-    }
+  render() {
+    return (
+      <button value={this.props.city} onClick={this.props.handleClick} className="city-button">
+        {this.props.city ? this.props.city : "All cities"}
+      </button>
+    );
+  }
 }
 
 export default Button;
