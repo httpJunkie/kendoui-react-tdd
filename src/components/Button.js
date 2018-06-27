cbc781b48faf8e014f1db6ca08d2a86d6286dcc1
Eric Bishard <e.bishard@gmail.com>
2018-06-26 20:24:14 -0700

    formatting and ternary changes

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,9 +4,9 @@ import './Button.css';
 class Button extends Component {
     render() {
         return (
-        <button value={this.props.city} onClick={this.props.handleClick} className="city-button">
-          {this.props.city ? this.props.city : "All cities"}
-        </button>
+          <button value={this.props.city} onClick={this.props.handleClick} className="city-button">
+            {this.props.city ? this.props.city : "All cities"}
+          </button>
       );
     }
 }
--- src/components/Map.js
+++ src/components/Map.js
@@ -3,12 +3,11 @@ import './Map.css';
 
 class Map extends Component {
   render() {
-    let imagePath;
-    if (this.props.imagename) {
-      imagePath = 'images/' + this.props.imagename;
-    } else {
-      imagePath = 'images/none.png';
-    }
+    let imagePath = 'images/' + (
+      (this.props.imagename)
+        ? this.props.imagename
+        : 'none.png'
+    );
     return (<div className="MapBox">
       <img src={imagePath} alt={this.props.city} />
     </div>);
