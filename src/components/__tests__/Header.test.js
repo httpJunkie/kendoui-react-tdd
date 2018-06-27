209e4ebc98cf4b62f28831c8594200804eaa3912
Eric Bishard <e.bishard@gmail.com>
2018-06-26 20:17:54 -0700

    formatting

--- src/components/__tests__/Header.test.js
+++ src/components/__tests__/Header.test.js
@@ -1,21 +1,21 @@
 import React from 'react';
-import {shallow} from 'enzyme';
+import { shallow } from 'enzyme';
 import Header from '../Header';
 
-describe("Header", function(){
+describe("Header", function () {
   let mountedHeader;
-  beforeEach(()=>{
+  beforeEach(() => {
     mountedHeader = shallow(<Header />);
   });
 
-    it('renders without crashing', () => {
-        shallow(<Header />);
-    });
+  it('renders without crashing', () => {
+    shallow(<Header />);
+  });
 
-    it('renders a logo',()=>{
-      const logoImg = mountedHeader.find('img [src="images/ebishard.png"]');
-      expect(logoImg.length).toBe(1);
-    });
+  it('renders a logo', () => {
+    const logoImg = mountedHeader.find('img [src="images/ebishard.png"]');
+    expect(logoImg.length).toBe(1);
+  });
 });
 
 
