01bbbed58c294a265afb39c3a9f903153f907158
Eric Bishard <e.bishard@gmail.com>
2018-06-26 19:36:27 -0700

    remove formatting

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react';
-//import logo from './logo.svg';
 
 class Header extends Component {
   render() {
     return (
       <div className="Header">
-
         <img src="images/ebishard.png" alt="Eric Bishard" />
       </div>
     );
